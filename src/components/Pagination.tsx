04ec59361aee58d11cd6195f03211725e92b8ddf
MAC <mac@MacBook-Pro-som-tillhor-MAC.local>
2025-03-31 23:01:29 +0330

    edit

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,7 +1,17 @@
 "use client"
 const Pagination = () => {
   return (
-    <div className=''>Pagination</div>
+    <div className='p-4 flex itens-center justify-between text-gray-500'>
+      <button disabled className="py-2 px-4 rounded-md bg-slate-200 text-xs font-demibold disabled:opacity-50 disabled:cursor-not-allowed" >Prev</button>
+      <div className="flex items-center gap-2 text-sm">
+        <button className="px-2 rounded-sm bg-se1aSky">1</button>
+        <button className="px-2 rounded-sm ">2</button>
+        <button className="px-2 rounded-sm">3</button>
+        .....
+        <button className="px-2 rounded-sm">10</button>
+      </div>
+      <button disabled className="py-2 px-4 rounded-md bg-slate-200 text-xs font-demibold disabled:opacity-50 disabled:cursor-not-allowed" >Next</button>
+    </div>
   )
 }
 
