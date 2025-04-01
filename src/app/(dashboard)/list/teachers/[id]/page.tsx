2a8991779ef9ee0fcb867d9b0d53ec31a21727eb
MAC <mac@MacBook-Pro-som-tillhor-MAC.local>
2025-04-01 23:37:30 +0330

    add

---
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -0,0 +1,25 @@
+const SingleTeacherPage = () => {
+  return (
+    <div className=' flex flex-1 p-4 flex-col gap-4 xl:flex-row'>
+        {/* LEFT */}
+        <div className="w-full xl:w-2/3">
+         {/* بالا */}
+         <div className=" flex flex-col lg:flex-row gap-4">
+
+        {/*user info card */}
+        <div className="bg-se1aSky py-6 px-4 rounded-md"></div>
+          {/*small card */}
+          <div className=""></div>
+
+         </div>
+         {/*bottom */}
+         <div className="">Schedule</div>
+        
+        </div>
+        {/* Right */}
+        <div className="w-full xl:w-1/3">r</div>
+    </div>
+  );
+};
+
+export default SingleTeacherPage;
