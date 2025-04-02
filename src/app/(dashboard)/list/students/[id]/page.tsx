d55c7e5eebdbf8c4ee5e5092835f39ed18923378
MAC <mac@MacBook-Pro-som-tillhor-MAC.local>
2025-04-02 20:06:04 +0330

    add

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -4,7 +4,7 @@ import Performance from "@/components/Performance";
 import Image from "next/image";
 import Link from "next/link";
 
-const SingleTeacherPage = () => {
+const SingleStudentPage = () => {
   return (
     <div className=' flex flex-1 p-4 flex-col gap-4 xl:flex-row'>
         {/* LEFT */}
@@ -15,10 +15,10 @@ const SingleTeacherPage = () => {
         {/*user info card */}
         <div className="bg-se1aSky py-6 px-4 rounded-md flex-1 flex gap-4">
           <div className="w-1/3">
-          <Image src="/profile.jpg" alt="cartImage" width={144} height={144} className="w-36 h-36 rounded-full object-cover" />
+          <Image src="/student.jpg" alt="cartImage" width={144} height={144} className="w-36 h-36 rounded-full object-cover" />
           </div>
           <div className="w-2/3 flex flex-col justify-between gap-4">
-          <h1 className="text-xl font-semibold">Sara Derakhsesh</h1>
+          <h1 className="text-xl font-semibold">Atrin moradi</h1>
           <p className="text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
             Voluptatum suscipit </p>
             <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
@@ -36,7 +36,7 @@ const SingleTeacherPage = () => {
               </div>
               <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                 <Image src="/phone.png" alt="" width={14} height={14}/>
-                <span>234,5677</span>
+                <span>234, 4564</span>
               </div>
             </div>
           </div>
@@ -55,16 +55,16 @@ const SingleTeacherPage = () => {
              <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image src="/singleBranch.png" alt="" width={24} height={24} className="w-6 h-6"/>
               <div className="">
-                <h1 className="text-xl font-semibold">2</h1>
-                <span className="text-sm text-gray-400">Branches</span>
+                <h1 className="text-xl font-semibold">6th</h1>
+                <span className="text-sm text-gray-400">Grade</span>
               </div>
             </div>
              {/*card */}
              <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image src="/singleLesson.png" alt="" width={24} height={24} className="w-6 h-6"/>
               <div className="">
-                <h1 className="text-xl font-semibold">6</h1>
-                <span className="text-sm text-gray-400">Lessons</span>
+                <h1 className="text-xl font-semibold">6A</h1>
+                <span className="text-sm text-gray-400">Class</span>
               </div>
             </div>
 
@@ -83,7 +83,7 @@ const SingleTeacherPage = () => {
          </div>
          {/*bottom */}
          <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
-          <h1>Teacher&apos;s Schedule</h1>
+          <h1>Student&apos;s Schedule</h1>
           <BigCalendar />
          </div>
         
@@ -93,11 +93,11 @@ const SingleTeacherPage = () => {
         <div className="bg-white p-4 rounded-md ">
           <h1 className="text-xl font-semibold">Shortcuts</h1>
           <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
-            <Link className="p-3 rounded-md bg-se1aSkyLight" href="/">Teacher&apos;s Classes</Link>
-            <Link className="p-3 rounded-md bg-se1aPurpleLight" href="/">Teacher&apos;s Students</Link>
-            <Link className="p-3 rounded-md bg-se1aYellowLight" href="/">Teacher&apos;s Lessons</Link>
-            <Link className="p-3 rounded-md bg-pink-50" href="/">Teacher&apos;s Exams</Link>
-            <Link className="p-3 rounded-md bg-se1aSkyLight" href="/">Teacher&apos;s Assignments</Link>
+            <Link className="p-3 rounded-md bg-se1aSkyLight" href="/">Student&apos;s Lessons</Link>
+            <Link className="p-3 rounded-md bg-se1aPurpleLight" href="/">Student&apos;s Teachers</Link>
+            <Link className="p-3 rounded-md bg-pink-50" href="/">Student&apos;s Exams</Link>
+            <Link className="p-3 rounded-md bg-se1aYellowLight" href="/">Student&apos;s Result</Link>
+            <Link className="p-3 rounded-md bg-se1aSkyLight" href="/">Student&apos;s Assignments</Link>
          
           </div>
         </div>
@@ -108,4 +108,4 @@ const SingleTeacherPage = () => {
   );
 };
 
-export default SingleTeacherPage;
+export default SingleStudentPage;
